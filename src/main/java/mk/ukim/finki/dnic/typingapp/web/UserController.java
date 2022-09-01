46abbcfbd954a7aa8896c1037a0398f2e6c30da7
Sara Misajlovska <Sara.Misajlovska@netcetera.com>
2022-09-01 15:33:46 +0200

    blank user controller created

---
+++ src/main/java/mk/ukim/finki/dnic/typingapp/web/UserController.java
@@ -0,0 +1,19 @@
+package mk.ukim.finki.dnic.typingapp.web;
+
+import mk.ukim.finki.dnic.typingapp.domain.identity.User;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin("http://localhost:3000")
+@RequestMapping("/user")
+public class UserController {
+
+//    @GetMapping
+//    public User getUser() {
+//
+//    }
+
+}
