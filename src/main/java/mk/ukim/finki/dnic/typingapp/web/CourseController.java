842cb9e508e96b69701ded8f59d1352083be1cb5
SaraMisajlovska <smisajlovska@gmail.com>
2022-09-03 21:14:11 +0200

    changed url to remote frontend

--- src/main/java/mk/ukim/finki/dnic/typingapp/web/CourseController.java
+++ src/main/java/mk/ukim/finki/dnic/typingapp/web/CourseController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin("http://localhost:3000")
+@CrossOrigin("https://typing-app-191517.herokuapp.com")
 @RequestMapping("/api/courses")
 @RequiredArgsConstructor
 public class CourseController {
--- src/main/java/mk/ukim/finki/dnic/typingapp/web/UserController.java
+++ src/main/java/mk/ukim/finki/dnic/typingapp/web/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin("http://localhost:3000")
+@CrossOrigin("https://typing-app-191517.herokuapp.com")
 @RequestMapping("/api/user")
 @AllArgsConstructor
 public class UserController {
